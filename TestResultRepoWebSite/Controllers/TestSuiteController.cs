094a1ba5e7ec29e5ea7d0340ce4fdac2c65add86
me919585 <me919585@TSELSE3614418>
2018-12-07 14:41:47 +0100

    Added controllers and views for testsuite and testrun. Changed links on Homepage

--- TestResultRepoWebSite/Controllers/TestSuiteController.cs
+++ TestResultRepoWebSite/Controllers/TestSuiteController.cs
@@ -12,35 +12,34 @@ using TestResultRepoData;
 
 namespace TestResultRepoWebSite.Controllers
 {
-    public class TestCaseController : Controller
+    public class TestSuiteController : Controller
     {
+        // GET: TestSuite
         string Baseurl = ConfigurationManager.AppSettings["APIBaseUrl"];
 
-        // GET: TestCase
         public async Task<ActionResult> Index(string Id)
         {
-            TestCase testCase = null;
+            TestSuite testSuite = null;
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(Baseurl);
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage response = await client.GetAsync($"api/testcase/{Id}");
+                HttpResponseMessage response = await client.GetAsync($"api/testsuite/{Id}");
 
                 if (response.IsSuccessStatusCode)
                 {
                     var json = response.Content.ReadAsStringAsync().Result;
-                    testCase = JsonConvert.DeserializeObject<List<TestCase>>(json).FirstOrDefault();
+                    testSuite = JsonConvert.DeserializeObject<List<TestSuite>>(json).FirstOrDefault();
 
-                    ViewBag.Message = "Here is the TestCase you were looking for:";
-                    ViewBag.TestCaseId = Id;
-                    if (testCase != null) ViewBag.TestCaseName = testCase.Name;
+                    ViewBag.Message = "Here is the TestSuite you were looking for:";
+                    ViewBag.TestSuiteId = Id;
+                    if (testSuite != null) ViewBag.TestSuiteName = testSuite.Name;
                 }
                 else
                 {
-                    ViewBag.Message = "No such TestCase found.";
+                    ViewBag.Message = "No such TestSuite found.";
                 }
-
             }
 
             return View();
