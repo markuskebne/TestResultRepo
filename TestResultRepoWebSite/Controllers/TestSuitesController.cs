78541d2beaecb0015155566f91d462a17d139bc7
me919585 <me919585@TSELSE3614418>
2018-12-18 14:49:30 +0100

    Added TestSuiteCollection cards to the suite list view

--- TestResultRepoWebSite/Controllers/TestSuitesController.cs
+++ TestResultRepoWebSite/Controllers/TestSuitesController.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using TestResultRepoData;
 
 namespace TestResultRepoWebSite.Controllers
 {
@@ -12,6 +16,8 @@ namespace TestResultRepoWebSite.Controllers
             if (id == null)
             {
                 var testSuites = await HelperMethods.TestResultRepoApiHelper.GetAllTestSuites();
+                var groupedSuites = getGroupedTestSuites(testSuites);
+
                 if (testSuites != null)
                 {
                     ViewBag.Message = "Here are the TestSuites you were looking for:";
@@ -22,7 +28,9 @@ namespace TestResultRepoWebSite.Controllers
                     ViewBag.Message = "No such TestSuite found.";
                 }
 
-                return View("All");
+                ViewBag.groups = groupedSuites;
+
+                return View("TestSuiteCollectionCard", groupedSuites);
             }
 
             // Return the index view if id is provided
@@ -54,5 +62,27 @@ namespace TestResultRepoWebSite.Controllers
 
             return View("TestSuiteCard", testSuite);
         }
+
+        private List<GroupedTestSuites> getGroupedTestSuites(List<TestSuite> testSuites)
+        {
+            var groupedTestSuites = new List<GroupedTestSuites>();
+            var uniqueSuiteNames = testSuites.Select(ts => ts.Name).Distinct();
+            foreach (var uniqueSuiteName in uniqueSuiteNames)
+            {
+                var group = new GroupedTestSuites();
+
+                group.Name = uniqueSuiteName;
+                group.TestSuites = testSuites.Where(ts => ts.Name == uniqueSuiteName).ToList();
+                group.Total = group.TestSuites.Count();
+                group.Passed = testSuites.Count(ts => ts.Result == Result.Passed);
+                group.Failed = testSuites.Count(ts => ts.Result == Result.Failed);
+
+                groupedTestSuites.Add(group);
+            }
+            
+            //var groupedList = testSuites.GroupBy(ts => ts.Name).Select(name => name.ToList()).ToList();
+
+            return groupedTestSuites;
+        } 
     }
 }
