6e59305b57e3f96c08b4918a9e709019d4889d20
me919585 <me919585@TSELSE3614418>
2019-02-18 10:28:54 +0100

    Refactoring

--- TestResultRepoWebSite/Controllers/AnalyzeController.cs
+++ TestResultRepoWebSite/Controllers/AnalyzeController.cs
@@ -27,55 +27,13 @@ namespace TestResultRepoWebSite.Controllers
                 }
             }
 
-            List<TestCaseFailure> failures = new List<TestCaseFailure>();
-            foreach (TestSuite suite in failingSuites)
-            {
-                foreach (TestCase testCase in suite.TestCases)
-                {
-                    failures.Add(new TestCaseFailure(testCase.Failure, testCase));
-                }
-            }
-
-            var groupedFailureList = failures
-                .GroupBy(f => f.Failure.Message)
-                .Select(grp => grp.ToList())
-                .ToList().OrderByDescending(g => g.Count).ToList();
-
             ViewBag.failingTestSuites = failingSuites;
 
             ViewBag.testRun = testRun;
             ViewBag.TestRunId = testRun._Id;
             ViewBag.TestRunName = testRun.Name;
-            ViewBag.GroupedFailures = groupedFailureList;
 
             return View();
         }
-
-        //public class TestCaseFailure
-        //{
-        //    public TestCaseFailure(Failure failure, TestCase testCase)
-        //    {
-        //        Failure = failure;
-        //        TestCase = testCase;
-        //    }
-
-        //    public Failure Failure { get; set; }
-
-        //    public TestCase TestCase { get; set; }
-        //}
-
-        //public class FailureGroup
-        //{
-        //    public FailureGroup(List<TestCaseFailure> failures, string name)
-        //    {
-        //        Failures = failures;
-        //        Name = name;
-        //    }
-
-        //    public List<TestCaseFailure> Failures { get; set; }
-
-        //    public string Name { get; set; }
-        //}
-
     }
 }
