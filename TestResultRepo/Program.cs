47612be9f153f1715c6ea1edc6e4836989c9189b
me919585 <markus.eriksson@telenor.se>
2019-07-10 09:45:34 +0200

    * The console application now saved testresults through the API instead of saving directly to Database

--- TestResultRepo/Program.cs
+++ TestResultRepo/Program.cs
@@ -1,26 +1,33 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
-using TestResultRepoIO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 using Output.Parsers;
-using static System.IO.Path;
+using TestResultRepoModels;
 
-namespace TestResultRepo
+namespace TestResultRepoConsole
 {
     class Program
     {
+        // The API endpoint url is configured in the app.config
+        private static string APIBaseUrl = ConfigurationManager.AppSettings["APIBaseUrl"];
+
         static void Main(string[] args)
         {
-            if (args.Length == 1)
+        if (args.Length == 1)
             {
                 // If argument is a nunit xml-file -> parse and save results
                 // ReSharper disable once PossibleNullReferenceException
-                if (GetExtension(args[0]).ToLower().Contains("xml"))
+                if (Path.GetExtension(args[0]).ToLower().Contains("xml"))
                 {
                     var filename = args[0];
                     var testRun = NunitParser.Parse(filename);
-                    MongoDb.SaveTestRun(testRun);
+                    SaveTestRun(testRun);
                 }
-                
+
                 // If argument is a folder
                 // if the folder contains nunit xml-files -> parse and save results
                 if (Directory.Exists(args[0]))
@@ -32,10 +39,10 @@ namespace TestResultRepo
 
                         foreach (var file in files)
                         {
-                            if (GetExtension(file).ToLower().Contains("xml"))
+                            if (Path.GetExtension(file).ToLower().Contains("xml"))
                             {
                                 var testRun = NunitParser.Parse(file);
-                                MongoDb.SaveTestRun(testRun);
+                                SaveTestRun(testRun);
                                 folderContainedXmlFiles = true;
                             }
                         }
@@ -60,20 +67,43 @@ namespace TestResultRepo
                 WriteUsageInfo();
             }
             
-            //var filename = "C:\\temp\\testresultrepo\\TestResult.xml";
-            //TestRun testRun = NunitParser.Parse(filename);
-
-            // Json.OutputToJson(testRun);
-
-            //MongoDB.SaveTestRun(testRun);
             Console.WriteLine("End program");
-            //Console.ReadLine();
         }
 
         private static void WriteUsageInfo()
         {
-            Console.WriteLine("[INFO] Usage 1:  TestResultRepo \"path-to-result-file\"");
-            Console.WriteLine("[INFO] Usage 2:  TestResultRepo \"path-to-folder-containing-multiple-result-files\"");
+            Console.WriteLine("[INFO] Usage 1:  TestResultRepo.exe \"path-to-result-file\"");
+            Console.WriteLine("[INFO] Usage 2:  TestResultRepo.exe \"path-to-folder-containing-multiple-result-files\"");
+            Console.ReadLine();
+        }
+
+        private static void SaveTestRun(TestRun testRun)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(APIBaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var content = new StringContent(JsonConvert.SerializeObject(testRun), Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = client.PostAsync("api/testrun/save", content);
+
+                    if (!response.Result.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("The request was sent successfully!");
+                    }
+
+                    Console.WriteLine($"Response code: {response.Result.StatusCode}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong when posting the result\nException: " + e);
+                }              
+            }
+
         }
     }
 }
